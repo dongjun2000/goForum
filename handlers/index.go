6356f8ab6ace075a9b75698f97027bf6b8187283
董俊 <418826102@qq.com>
2022-06-24 18:00:52 +0800

    完成首页

--- handlers/index.go
+++ handlers/index.go
@@ -1,10 +1,16 @@
 package handlers
 
 import (
-	"fmt"
+	"goForum/models"
+	"html/template"
 	"net/http"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello GoWeb~~!")
+	files := []string{"views/layout.html", "views/navbar.html", "views/index.html"}
+	templates := template.Must(template.ParseFiles(files...))
+	threads, err := models.Threads()
+	if err == nil {
+		templates.ExecuteTemplate(w, "layout", threads)
+	}
 }
